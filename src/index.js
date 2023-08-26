d0294a9e490f8cdc8cb842bee922fb34b052d2ea
Poonam229 <poonam.chauhan229@gmail.com>
2023-08-26 11:50:36 +0530

    express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const products = require('./products');
-const express = require('express');
+// const express = require('express');
 const cors = require('cors');
 
 const dotenv = require('dotenv');
